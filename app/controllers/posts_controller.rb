cb503d08f681099ee6eaabe38c99057ce0bb1b7e
jongwonIee <ljw3728@gmail.com>
2017-04-14 22:06:21 +0900

    new post works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
